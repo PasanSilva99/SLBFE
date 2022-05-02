44b2d92481f0b520c8a067089e5e6118c1a26e0f
Pasan Anuththara <pasananuththara19@gmail.com>
2022-05-02 13:22:55 +0530

    Global Config

--- SLBFE_API/SLBFE/Global.asax.cs
+++ SLBFE_API/SLBFE/Global.asax.cs
@@ -18,6 +18,8 @@ namespace SLBFE
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+
         }
+
     }
 }
