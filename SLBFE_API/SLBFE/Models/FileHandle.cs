85ef30d906c8abf8464fd86a8eb3dac02de79290
Pasan Anuththara <pasananuththara19@gmail.com>
2022-05-11 13:43:17 +0530

    File Handle

---
+++ SLBFE_API/SLBFE/Models/FileHandle.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SLBFE.Models
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class FileHandle
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        public string Name { get; set; }
+        /// <summary>
+        /// 
+        /// </summary>
+        public string Path { get; set; }
+        /// <summary>
+        /// 
+        /// </summary>
+        public byte[] Data { get; set; }
+    }
+}
