169ea9e3e3230d978df590227efdc4e7a1e4cffa
Pasan Anuththara <pasananuththara19@gmail.com>
2022-05-11 11:10:14 +0530

    Complaint Controller Update

--- SLBFE_API/SLBFE/Controllers/ComplaintController.cs
+++ SLBFE_API/SLBFE/Controllers/ComplaintController.cs
@@ -7,6 +7,9 @@ using System.Web.Http;
 
 namespace SLBFE.Controllers
 {
+    /// <summary>
+    /// 
+    /// </summary>
     public class ComplaintController : ApiController
     {
         /// <summary>
@@ -14,9 +17,46 @@ namespace SLBFE.Controllers
         /// </summary>
         /// <returns>Complaints List</returns>
         // GET: api/Complaint
-        public List<Models.Complaint> Get()
+        public List<Models.Complaint> GetAllComplaints()
         {
             return Models.DataStore.GetComplaints();
+        }
+
+        /// <summary>
+        /// Gets complaint as 10 for a page
+        /// </summary>
+        /// <param name="page">Page Number</param>
+        /// <returns>10 objects that bel;ongs to that page</returns>
+        [Route("api/Complaint/Page")]
+        [HttpGet]
+        public List<Models.Complaint> GetComplaints(int page)
+        {
+            var complaintList = Models.DataStore.GetComplaints();
+
+            if (complaintList.Count < page * 10)
+            {
+                // if it is no values on the page
+                // send an empty list
+                // this can be detected by the client and show no complaintas left
+                return new List<Models.Complaint>();
+            }
+            else if (complaintList.Count < page * 10 + 10)
+            {
+                // this means there are no values for 10 from this page
+                // to avoid the index out of range error, 
+                // we will get how many values left fitst
+
+                var listCount = complaintList.Count;
+                var index = page * 10;
+                var valuesLeft = listCount - index; //count 56/ Requested page 5 = 50 / 56-50 = 50
+
+                return complaintList.GetRange(page * 10, valuesLeft);
+            }
+            else
+            {
+                return complaintList.GetRange(page * 10, 10);
+            }
+
         }
 
         /// <summary>
@@ -26,14 +66,58 @@ namespace SLBFE.Controllers
         /// <param name="email">Email of the User/ Company</param>
         /// <returns></returns>
         // GET: api/Complaint/email.sample.com
-        public List<Models.Complaint> Get(string email)
+        public List<Models.Complaint> GetComplaintsFor(string email)
         {
             var complaints = Models.DataStore.GetComplaints();
             return complaints.Where(c => c.Email == email).ToList();
         }
 
+        /// <summary>
+        /// This will return the complaints as a list
+        /// where the email is equal
+        /// And 10 for a page
+        /// </summary>
+        /// <param name="email">Email of the User/ Company</param>
+        /// <returns></returns>
+        // GET: api/Complaint/email.sample.com
+        [Route("api/Complaints/{email}/{page}")]
+        [HttpGet]
+        public List<Models.Complaint> GetComplaintsFor(string email, int page)
+        {
+            var complaints = Models.DataStore.GetComplaints();
+
+            if (complaints.Count < page * 10)
+            {
+                // if it is no values on the page
+                // send an empty list
+                // this can be detected by the client and show no complaintas left
+                return new List<Models.Complaint>();
+            }
+            else if (complaints.Count < page * 10 + 10)
+            {
+                // this means there are no values for 10 from this page
+                // to avoid the index out of range error, 
+                // we will get how many values left fitst
+
+                var listCount = complaints.Count;
+                var index = page*10;
+                var valuesLeft = listCount - index; //count 56/ Requested page 5 = 50 / 56-50 = 50
+
+                return complaints.Where(c => c.Email == email).ToList().GetRange(page * 10, valuesLeft);
+            }
+            else
+            {
+                return complaints.Where(c => c.Email == email).ToList().GetRange(page * 10, 10);
+            }
+        }
+
+        /// <summary>
+        /// Add a new Complaint
+        /// </summary>
+        /// <param name="value">Complaint Object</param>
+        /// <returns>Status 1 for success 0 for failed</returns>
         // POST: api/Complaint
-        public int Post([FromBody]Models.Complaint value)
+        public int NewComplaint([FromBody]Models.Complaint value)
         {
             return Models.DataStore.NewComplaint(value);
         }
@@ -43,23 +127,34 @@ namespace SLBFE.Controllers
         /// </summary>
         /// <param name="id"></param>
         /// <param name="value"></param>
-        /// <returns></returns>
+        /// <returns>0 - Failed, 1 - Success, -1 - Failed due to DB Error, -2 Failed due to input data or db connection error</returns>
         // PUT: api/Complaint/5
-        public int Put(string id, [FromBody]Models.ComplaintReply value)
+        public int NewReply(string id, [FromBody]Models.ComplaintReply value)
         {
             return Models.DataStore.ReplyComplaint(id, value);
         }
 
+        /// <summary>
+        /// Deletes the complaints from the database
+        /// </summary>
+        /// <param name="id">Complaint ID</param>
+        /// <returns>0 - Failed, 1 - Success, -1 - Failed due to DB Error, -2 Failed due to input data or db connection error</returns>
         // DELETE: api/Complaint/5
-        public int Delete(string id)
+        public int DeleteComplaint(string id)
         {
             return Models.DataStore.DeleteComplaint(id);
         }
 
+        /// <summary>
+        /// Deletes the Reply from th3 database
+        /// </summary>
+        /// <param name="complaintID">Unique ID of the complaint</param>
+        /// <param name="replyID">Unique ID of the Reply</param>
+        /// <returns>0 - Failed, 1 - Success, -1 - Failed due to DB Error, -2 Failed due to input data or db connection error</returns>
         // DELETE: api/Complaint/5
-        public int Delete(string id, string replyID)
+        public int DeleteReply(string complaintID, string replyID)
         {
-            return Models.DataStore.DeleteComplaintReply(id, replyID);
+            return Models.DataStore.DeleteComplaintReply(complaintID, replyID);
         }
     }
 }
