1aa2f2334fbf2f1df9b02e88d92190ee58a663b2
Sandaru D Silva <sandarudsilva.dev@gmail.com>
2022-05-11 10:25:15 +0530

    Bureau Controller Summaries Updated!

--- SLBFE_API/SLBFE/Controllers/BureauController.cs
+++ SLBFE_API/SLBFE/Controllers/BureauController.cs
@@ -19,9 +19,9 @@ namespace SLBFE.Controllers
         //}
 
         /// <summary>
-        /// Gets a list of available Bureau Officers
+        /// Gets a list of Available Bureau Officers
         /// </summary>
-        /// <returns></returns>
+        /// <returns>Returns a list of Available Bureau Officers</returns>
         // GET: api/Bureau/5
         public List<Models.Bureau> Get()
         {
@@ -39,10 +39,10 @@ namespace SLBFE.Controllers
         }
         
         /// <summary>
-        /// Returns the Officer Details According to the NationalID 
+        /// Checking the Officer Details According to the NationalID 
         /// </summary>
         /// <param name="nationalID"></param>
-        /// <returns></returns>
+        /// <returns>Returns Officers Details Related to the Given NationalID</returns>
         public List<Models.Bureau> Get(string nationalID)
         {
             var list = new List<Models.Bureau>();
@@ -58,6 +58,36 @@ namespace SLBFE.Controllers
                 }
 
                 return list.Where(b => b.NationalID == nationalID).ToList();
+            }
+            else
+            {
+                return new List<Models.Bureau>();
+            }
+        }
+
+
+        /// <summary>
+        /// Get the Officer that Belongs to Entered Email
+        /// </summary>
+        /// <param name="email"></param>
+        /// <returns>Returns a List Related to the Entered Email</returns>
+        [Route("api/FindOfficer")]
+        [HttpGet]
+        public List<Models.Bureau> GetOfficerFromEmail(string email)
+        {
+            var list = new List<Models.Bureau>();
+            var officerlist = Models.DataStore.GetBureaus();
+
+            if (officerlist != null)
+            {
+                foreach (var officer in officerlist)
+                {
+                    var offi = officer;
+                    offi.Password = "";
+                    list.Add(offi);
+                }
+
+                return list.Where(b => b.Email == email).ToList();
             }
             else
             {
@@ -91,22 +121,33 @@ namespace SLBFE.Controllers
 
 
         /// <summary>
-        /// Updates the citizen's validation
+        /// Updates the Citizen's Validation
         /// </summary>
         /// <param name="validationData"></param>
-        /// <returns></returns>
+        /// <returns>Returns the Validated Citizen</returns>
         // POST: api/Bureau
         public int Post([FromBody] Models.UserValidation validationData)
         {
             return Models.DataStore.ValidateCitizen(validationData);
         }
-
-
+        
+        
+        /// <summary>
+        /// Checking Whether Entered Email Belongs to an Officer
+        /// </summary>
+        /// <param name="email"></param>
+        /// <returns>Returns Whether the Entered Email is Belongs to a Officer</returns>
+        [Route("api/isOfficer")]
+        [HttpGet]
+        public int isOfficer(string email)
+        {
+            return Models.DataStore.IsOfficer(email);
+        }
 
         /// <summary>
         /// DELETE Request
         /// </summary>
-        /// <param name="nationalID">National ID of the officer that need to deleted</param>
+        /// <param name="nationalID">NationalID of the Officer that Needs to Delete</param>
         /// <param name="requestedBy">The officer that deleted the account</param>
         // DELETE: api/Bureau/5
         public void Delete(string nationalID, string requestedBy)
